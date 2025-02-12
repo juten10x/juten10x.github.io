b08a88cb02acdc97c6873f2caa6fd44b941d52a6
juten10x <juten10x@gmail.com>
2025-02-12 10:13:33 +0900

    fix, time

--- assets/last-md.js
+++ assets/last-md.js
@@ -1,3 +1,3 @@
 const t = new Date(document.lastModified);
 const day = t.getFullYear() + "-" + ('0' + (t.getMonth() + 1)).slice(-2) + "-" + ('0' + (t.getDate())).slice(-2);
-document.getElementById('lastModifiedDay').textContent = day;
+document.getElementById('lastModifiedDay').innerHTML = "<time datetime=\"" + day + "\">" + day + "</time>";
